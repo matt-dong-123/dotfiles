1960f94127f43a4ac4f5d88109d5629a239c6502
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2024-12-30 18:52:19 +0800

    updated nvim config

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,8 +1,8 @@
 return {
     { "ThePrimeagen/vim-be-good" },
-    { "stevearc/oil.nvim" },
     { "tpope/vim-fugitive" },
     { "tpope/vim-commentary" },
     { "tribela/vim-transparent" },
     { "tpope/vim-surround" },
+    { "HiPhish/rainbow-delimiters.nvim" },
 }
