00b60a49e7f05678f0cb146e23fb21d149b0312d
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-11 23:58:40 +0800

    added snacks dashboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -244,6 +244,63 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'folke/snacks.nvim',
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = 'header',
+            hl = 'SpecialKey',
+          },
+          {
+            section = 'keys',
+            gap = 1,
+            padding = 1,
+          },
+          {
+            section = 'startup',
+          },
+        },
+        formats = {
+          icon = function(item)
+            if item.file and item.icon == 'file' or item.icon == 'directory' then
+              return M.icon(item.file, item.icon)
+            end
+            return { item.icon, width = 2, hl = 'icon' }
+          end,
+          footer = { '%s', align = 'center' },
+          header = { '%s', align = 'center' },
+          file = function(item, ctx)
+            local fname = vim.fn.fnamemodify(item.file, ':~')
+            fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+            if #fname > ctx.width then
+              local dir = vim.fn.fnamemodify(fname, ':h')
+              local file = vim.fn.fnamemodify(fname, ':t')
+              if dir and file then
+                file = file:sub(-(ctx.width - #dir - 2))
+                fname = dir .. '/' .. file
+              end
+            end
+            return fname
+          end,
+        },
+        keys = {
+          { icon = '􀬸', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
+          { icon = '􀣗', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = '􀕹', key = 'g', desc = 'Find Text', action = ':Telescope live_grep' },
+          { icon = '􀐫', key = 'r', desc = 'Recent Files', action = ':Telescope oldfiles' },
+          { icon = '􀍟', key = 'c', desc = 'Config', action = ':Telescope find_files cwd=$XDG_CONFIG_HOME/nvim' },
+          { icon = '􀅉', key = 's', desc = 'Restore Session', section = 'session' },
+          { icon = '􀆽', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+          { icon = '􀁠', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+        header = [[
+  ███╗ ██╗███████╗ ██████╗ ██╗ ██╗██╗███╗ ███╗ ████╗ ██║██╔════╝██╔═══██╗██║ ██║██║████╗ ████║ ██╔██╗ ██║█████╗ ██║ ██║██║ ██║██║██╔████╔██║ ██║╚██╗██║██╔══╝ ██║ ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ╚═╝ ╚═══╝╚══════╝ ╚═════╝ ╚═══╝ ╚═╝╚═╝ ╚═╝
+  ]],
+      },
+    },
+  },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
