28e194dfb8fcba811963512b206b396362cacbfc
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-12 19:13:49 +0800

    replaced vim-orgmode with nvim-orgmode

--- .config/nvim/lua/custom/plugins/orgmode.lua
+++ .config/nvim/lua/custom/plugins/orgmode.lua
@@ -1,3 +1,27 @@
 return {
-  'jceb/vim-orgmode',
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = 'org',
+    config = function()
+        require('orgmode').setup {
+            org_agenda_files = { '~/org/agenda.org' },
+            org_default_notes_file = '~/org/notes.org',
+            org_archive_files = { '~/org/archive.org' },
+            org_span_tags = {
+                'TODO',
+                'NEXT',
+                'DONE',
+                'WAITING',
+                'HOLD',
+                'CANCELLED',
+                'NOPRIORITY',
+                'A',
+                'B',
+                'C',
+            },
+            org_heading_delimiter = '-',
+            org_list_bullets = '*-+',
+            org_date_link_format = '(%s)',
+        }
+    end,
 }
