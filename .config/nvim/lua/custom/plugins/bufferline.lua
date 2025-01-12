000866350ae6d1e8a1cbf4176ea5ac3fd238baf2
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-12 17:44:37 +0800

    fixed issue where buffer_close_icon wouldn't show

--- .config/nvim/lua/custom/plugins/bufferline.lua
+++ .config/nvim/lua/custom/plugins/bufferline.lua
@@ -10,7 +10,7 @@ return {
                 left_mouse_command = 'buffer %d',
                 middle_mouse_command = nil,
                 indicator_icon = '▎',
-                buffer_close_icon = '',
+                buffer_close_icon = '',
                 modified_icon = '●',
                 close_icon = '',
                 left_trunc_marker = '',
