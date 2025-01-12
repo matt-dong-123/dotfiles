4f5d51cdada9f945cd22affdbf6ebd98641c7966
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-12 22:32:23 +0800

    added inc-rename keybinding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,6 +108,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Inc-rename keymaps
+vim.keymap.set('n', '<leader>rn', ':IncRename ')
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
