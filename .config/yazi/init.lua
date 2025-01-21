d6ebc64e76118e8af53c240c3fb45a0f5d71f170
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-21 16:01:03 +0800

    set up relative motions

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -7,4 +7,6 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
+require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true })
+
 require("git"):setup()
