60c983c6e20e6783a623eb4287ec74c276d9ee36
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-22 23:17:41 +0800

    added icons for future use

--- .config/sketchybar/icons.lua
+++ .config/sketchybar/icons.lua
@@ -1,91 +1,98 @@
 local settings = require("settings")
 
 local icons = {
-  sf_symbols = {
-    plus = "􀅼",
-    loading = "􀖇",
-    apple = "􀣺",
-    gear = "􀍟",
-    cpu = "􀫥",
-    clipboard = "􀉄",
-    switch = {
-      on = "􁏮",
-      off = "􁏯",
-    },
-    volume = {
-      _100="􀊩",
-      _66="􀊧",
-      _33="􀊥",
-      _10="􀊡",
-      _0="􀊣",
-    },
-    battery = {
-      _100 = "􀛨",
-      _75 = "􀺸",
-      _50 = "􀺶",
-      _25 = "􀛩",
-      _0 = "􀛪",
-      charging = "􀢋"
-    },
-    wifi = {
-      upload = "􀄨",
-      download = "􀄩",
-      connected = "􀙇",
-      disconnected = "􀙈",
-      router = "􁓤",
-    },
-    media = {
-      back = "􀊊",
-      forward = "􀊌",
-      play_pause = "􀊈",
-    },
-  },
+	sf_symbols = {
+		browser = "􀆪",
+		terminal = "􀩼",
+		music = "􀑪",
+		social = "􁒃",
+		work = "􁂥",
+		plus = "􀅼",
+		loading = "􀖇",
+		apple = "􀣺",
+		gear = "􀍟",
+		cpu = "􀫥",
+		clipboard = "􀉄",
+		switch = {
+			on = "􁏮",
+			off = "􁏯",
+		},
+		volume = {
+			_100 = "􀊩",
+			_66 = "􀊧",
+			_33 = "􀊥",
+			_10 = "􀊡",
+			_0 = "􀊣",
+		},
+		battery = {
+			_100 = "􀛨",
+			_75 = "􀺸",
+			_50 = "􀺶",
+			_25 = "􀛩",
+			_0 = "􀛪",
+			charging = "􀢋",
+		},
+		wifi = {
+			upload = "􀄨",
+			download = "􀄩",
+			connected = "􀙇",
+			disconnected = "􀙈",
+			router = "􁓤",
+		},
+		media = {
+			back = "􀊊",
+			forward = "􀊌",
+			play_pause = "􀊈",
+		},
+	},
 
-  -- Alternative NerdFont icons
-  nerdfont = {
-    plus = "",
-    loading = "",
-    apple = "",
-    gear = "",
-    cpu = "",
-    clipboard = "Missing Icon",
+	-- Alternative NerdFont icons
+	nerdfont = {
+		browser = "🌐",
 
-    switch = {
-      on = "󱨥",
-      off = "󱨦",
-    },
-    volume = {
-      _100="",
-      _66="",
-      _33="",
-      _10="",
-      _0="",
-    },
-    battery = {
-      _100 = "",
-      _75 = "",
-      _50 = "",
-      _25 = "",
-      _0 = "",
-      charging = ""
-    },
-    wifi = {
-      upload = "",
-      download = "",
-      connected = "󰖩",
-      disconnected = "󰖪",
-      router = "Missing Icon"
-    },
-    media = {
-      back = "",
-      forward = "",
-      play_pause = "",
-    },
-  },
+		plus = "",
+		loading = "",
+		apple = "",
+		gear = "",
+		cpu = "",
+		clipboard = "Missing Icon",
+
+		switch = {
+			on = "󱨥",
+			off = "󱨦",
+		},
+		volume = {
+			_100 = "",
+			_66 = "",
+			_33 = "",
+			_10 = "",
+			_0 = "",
+		},
+		battery = {
+			_100 = "",
+			_75 = "",
+			_50 = "",
+			_25 = "",
+			_0 = "",
+			charging = "",
+		},
+		wifi = {
+			upload = "",
+			download = "",
+			connected = "󰖩",
+			disconnected = "󰖪",
+			router = "Missing Icon",
+		},
+		media = {
+			back = "",
+			forward = "",
+			play_pause = "",
+		},
+	},
 }
 
 if not (settings.icons == "NerdFont") then
-  return icons.sf_symbols
+	return icons.sf_symbols
 else
-  return icons.nerdfont
+	return icons.nerdfont
 end
