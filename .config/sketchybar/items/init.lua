b6f0e6a91dbed828bd5d83c4b7da3347429d3df6
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-23 14:08:39 +0800

    repositioned media widget

--- .config/sketchybar/items/init.lua
+++ .config/sketchybar/items/init.lua
@@ -1,7 +1,7 @@
 require("items.apple")
+require("items.media")
 require("items.front_app")
 require("items.menus")
 require("items.aerospace")
 require("items.calendar")
 require("items.widgets")
-require("items.media")
