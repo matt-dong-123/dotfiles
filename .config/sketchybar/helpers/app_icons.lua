d5a7bfbf05fcd2ccfa390470776520af6009d2c7
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-28 19:43:52 +0800

    added icon

--- .config/sketchybar/helpers/app_icons.lua
+++ .config/sketchybar/helpers/app_icons.lua
@@ -57,6 +57,7 @@ return {
 	["Affinity Photo 2"] = ":affinity_photo_2:",
 	["Music"] = ":music:",
 	["音乐"] = ":music:",
+	["QQ音乐"] = ":music:",
 	["League of Legends"] = ":league_of_legends:",
 	["Tor Browser"] = ":tor_browser:",
 	["Hyper"] = ":hyper:",
