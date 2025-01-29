ff25fa84f0bd9f3aea6e77f88cdb792b903af7e9
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-29 20:30:52 +0800

    refactored wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,27 +1,17 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
-config.color_scheme = "Tokyo Night"
-
-config.font = wezterm.font_with_fallback({ "Maple Mono NF CN", "JetBrainsMono Nerd Font", "Consolas" })
-config.font_size = 20
-
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 100
-
-config.default_cursor_style = "BlinkingBar"
-config.cursor_blink_rate = 500
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-
-config.max_fps = 120
--- and finally, return the configuration to wezterm
+config = {
+	color_scheme = "Tokyo Night",
+	font = wezterm.font_with_fallback({ "Maple Mono NF CN", "JetBrainsMono Nerd Font", "Consolas" }),
+	font_size = 20,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.75,
+	macos_window_background_blur = 100,
+	default_cursor_style = "BlinkingBar",
+	cursor_blink_rate = 500,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	max_fps = 120,
+}
 return config
