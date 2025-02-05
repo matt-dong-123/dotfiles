51e977e1cb886d52f280b4bf437d47df4bd8fe74
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-02-06 00:21:48 +0800

    removed nvim-cmp

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -11,7 +11,7 @@ return {
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
         'rcarriga/nvim-notify',
-        'hrsh7th/nvim-cmp',
+        -- 'hrsh7th/nvim-cmp',
     },
     config = function()
         require('noice').setup {
@@ -20,7 +20,7 @@ return {
                 override = {
                     ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
                     ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+                    -- ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
                 },
             },
             -- you can enable a preset for easier configuration
