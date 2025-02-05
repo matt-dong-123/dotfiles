eb888e6461477c0d99eb092a6654acc2540b057f
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-02-05 14:13:48 +0800

    added icon

--- .config/sketchybar/items/front_app.lua
+++ .config/sketchybar/items/front_app.lua
@@ -1,8 +1,13 @@
 local settings = require("settings")
+local app_icons = require("helpers.app_icons")
+local colors = require("colors")
 
 local front_app = sbar.add("item", "front_app", {
 	display = "active",
-	icon = { drawing = false },
+	icon = {
+		font = "sketchybar-app-font:Regular:16.0",
+		color = colors.default,
+	},
 	label = {
 		font = {
 			size = 20.0,
@@ -13,7 +18,10 @@ local front_app = sbar.add("item", "front_app", {
 })
 
 front_app:subscribe("front_app_switched", function(env)
-	front_app:set({ label = { string = env.INFO } })
+	front_app:set({
+		icon = { string = app_icons[env.INFO] },
+		label = { string = env.INFO },
+	})
 end)
 
 front_app:subscribe("mouse.clicked", function(env)
