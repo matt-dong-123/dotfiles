39077d39e9e5006f858ca8707dde7d16f20ec176
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-02-14 12:50:36 +0800

    updated yazi plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -10,3 +10,11 @@ require("full-border"):setup({
 require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true })
 
 require("git"):setup()
+
+THEME.git = THEME.git or {}
+THEME.git.modified_sign = "M"
+THEME.git.deleted_sign = "D"
+THEME.git.added_sign = "A"
+THEME.git.untracked_sign = "?"
+THEME.git.updated_sign = "U"
+THEME.git.ignored_sign = "I"
--- .config/yazi/plugins/git.yazi/main.lua
+++ .config/yazi/plugins/git.yazi/main.lua
@@ -1,3 +1,5 @@
+--- @since 25.2.7
+
 local WIN = ya.target_family() == "windows"
 local PATS = {
 	{ "[MT]", 6 }, -- Modified
@@ -120,19 +122,19 @@ local function setup(st, opts)
 	-- Chosen by ChatGPT fairly, PRs are welcome to adjust them
 	local t = THEME.git or {}
 	local styles = {
-		[6] = t.modified and ui.Style(t.modified) or ui.Style():fg("#ff9e64"),
-		[5] = t.added and ui.Style(t.added) or ui.Style():fg("#1abc9c"),
-		[4] = t.untracked and ui.Style(t.untracked) or ui.Style():fg("#a9b1d6"),
-		[3] = t.ignored and ui.Style(t.ignored) or ui.Style():fg("#545c7e"),
-		[2] = t.deleted and ui.Style(t.deleted) or ui.Style():fg("#ff007c"),
-		[1] = t.updated and ui.Style(t.updated) or ui.Style():fg("#0db9d7"),
+		[6] = t.modified and ui.Style(t.modified) or ui.Style():fg("#ffa500"),
+		[5] = t.added and ui.Style(t.added) or ui.Style():fg("#32cd32"),
+		[4] = t.untracked and ui.Style(t.untracked) or ui.Style():fg("#a9a9a9"),
+		[3] = t.ignored and ui.Style(t.ignored) or ui.Style():fg("#696969"),
+		[2] = t.deleted and ui.Style(t.deleted) or ui.Style():fg("#ff4500"),
+		[1] = t.updated and ui.Style(t.updated) or ui.Style():fg("#1e90ff"),
 	}
 	local signs = {
-		[6] = t.modified_sign and t.modified_sign or "M",
-		[5] = t.added_sign and t.added_sign or "A",
-		[4] = t.untracked_sign and t.untracked_sign or "?",
-		[3] = t.ignored_sign and t.ignored_sign or "I",
-		[2] = t.deleted_sign and t.deleted_sign or "D",
+		[6] = t.modified_sign and t.modified_sign or "",
+		[5] = t.added_sign and t.added_sign or "",
+		[4] = t.untracked_sign and t.untracked_sign or "",
+		[3] = t.ignored_sign and t.ignored_sign or "",
+		[2] = t.deleted_sign and t.deleted_sign or "",
 		[1] = t.updated_sign and t.updated_sign or "U",
 	}
 
@@ -145,11 +147,11 @@ local function setup(st, opts)
 		end
 
 		if not change or signs[change] == "" then
-			return ui.Line("")
+			return ""
 		elseif self._file:is_hovered() then
-			return ui.Line({ ui.Span(" "), ui.Span(signs[change]) })
+			return ui.Line { " ", signs[change] }
 		else
-			return ui.Line({ ui.Span(" "), ui.Span(signs[change]):style(styles[change]) })
+			return ui.Line { " ", ui.Span(signs[change]):style(styles[change]) }
 		end
 	end, opts.order)
 end
@@ -159,7 +161,7 @@ local function fetch(_, job)
 	local repo = root(cwd)
 	if not repo then
 		remove(tostring(cwd))
-		return 1
+		return true
 	end
 
 	local paths = {}
@@ -175,8 +177,7 @@ local function fetch(_, job)
 		:stdout(Command.PIPED)
 		:output()
 	if not output then
-		ya.err("Cannot spawn git command, error: " .. err)
-		return 0
+		return true, Err("Cannot spawn `git` command, error: %s", err)
 	end
 
 	local changed, ignored = {}, {}
@@ -202,7 +203,7 @@ local function fetch(_, job)
 	end
 	add(tostring(cwd), repo, changed)
 
-	return 3
+	return false
 end
 
 return { setup = setup, fetch = fetch }
