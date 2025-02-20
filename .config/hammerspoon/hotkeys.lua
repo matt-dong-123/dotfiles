3bfe1e5dea5047008a03f7f14ba8c1205d1693d2
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-02-20 18:56:02 +0800

    refactored hammerspoon config

---
+++ .config/hammerspoon/hotkeys.lua
@@ -0,0 +1,41 @@
+-- Fuck you lsp
+hs = hs
+
+-- hyper key & meh key
+local hypr = { "ctrl", "alt", "cmd", "shift" }
+local meh = { "ctrl", "alt", "shift" }
+
+-- Apps
+local hotkeys = {
+	a = "Firefox",
+	d = "Dorico 5",
+	f = "Finder",
+	l = "Logic Pro",
+	m = "MuseScore 4",
+	n = "Obsidian",
+	o = "wpsoffice",
+	p = "Passwords",
+	q = "QQMusic",
+	s = "System Settings",
+	t = "Tor Browser",
+	v = "VSCodium",
+	w = "WeChat",
+	z = "zoom.us",
+	["return"] = "Ghostty",
+}
+
+local function bind_hotkeys(mapping)
+	for key, app in pairs(mapping) do
+		hs.hotkey.bind(hypr, key, function()
+			hs.application.launchOrFocus(app)
+		end)
+	end
+	hs.hotkey.bind(meh, "return", function()
+		hs.application.launchOrFocus("Emacs")
+	end)
+	hs.hotkey.bind("ctrl", "space", function()
+		hs.application.launchOrFocus("Launchpad")
+	end)
+end
+
+bind_hotkeys(hotkeys)
