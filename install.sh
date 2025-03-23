4be0cd83bba820f8815b77629bf3853a2badbef2
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-23 22:12:01 +0800

    (MAJOR) overhaul of install script

--- install.sh
+++ install.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
+# Colors!
+green="\033[1;32m"
+yellow="\033[1;33m"
+red="\033[1;31m"
+purple="\033[1;35m"
+blue="\033[1;34m"
+no="\033[0m"
+
 # Install xCode cli tools
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo "macOS deteted..."
+    echo -e "${yellow}macOS deteted...${no}"
 
     if xcode-select -p &>/dev/null; then
-        echo "Xcode already installed"
+        echo -e "${yellow}Xcode already installed${no}"
     else
-        echo "Installing commandline tools..."
+        echo -e "${green}Installing commandline tools...${no}"
         xcode-select --install
     fi
 fi
 
-# Install oh-my-zsh
-echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-## install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-## install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-## install fzf-tab
-git clone https://github.com/Aloxaf/fzf-tab.git ~/.oh-my-zsh/custom/plugins/fzf-tab
-## install powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 # Homebrew
 ## Install
-echo "Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew analytics off
-
+if [ "$(which brew)" != "/usr/local/bin/brew" ] && [ "$(which brew)" != "/opt/homebrew/bin/brew" ]; then
+    echo -e "${green}Installing Brew...${no}"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew analytics off
+fi
 ## Taps
-echo "Tapping Brew..."
-brew tap homebrew/services
+echo -e "${green}Tapping Brew...${no}"
 brew tap nikitabobko/tap
 brew tap dimentium/autoraise
 brew tap FelixKratz/formulae
@@ -39,7 +36,7 @@ brew tap lihaoyun6/tap
 brew tap mhaeuser/mhaeuser
 
 ## Formulae
-echo "Installing Brew Formulae... (Press C-c to cancel installing any of the formulae you don't want)"
+echo -e "${green}Installing Brew Formulae... ${no}"
 ### Must Have things
 brew install stow
 brew install fzf
@@ -81,7 +78,6 @@ brew install sl
 brew install yt-dlp
 brew install wtf
 brew install pipes-sh
-brew install neovide
 brew install pnpm
 
 ### dev things
@@ -91,31 +87,19 @@ brew install sqlite
 
 ## Casks (you'll have to configure them yourselves)
 # wait till i learn nix :)
-echo "Installing Brew Casks... (Press C-c to cancel installing any of the casks you don't want)"
+echo -e "${green}Installing Brew Casks... ${no}"
 brew install --cask aerospace
 brew install --cask karabiner-elements
 brew install --cask hammerspoon
 brew install --cask espanso
 brew install --cask battery-toolkit
-read -p "Choose a terminal emulator to install: (BOTH/wezterm/ghostty/none) " terminal
-if [ "$terminal" = "wezterm" ]; then
-    echo "Installing wezterm..."
-    brew install --cask wezterm
-elif [ "$terminal" = "ghostty" ]; then
-    echo "Installing ghostty..."
-    brew install --cask ghostty
-elif [ "$terminal" = "none" ]; then
-    echo "No terminal emulator will be installed"
-else
-    echo "Installing both..."
-    brew install --cask wezterm
-    brew install --cask ghostty
-fi
+brew install --cask wezterm
 
 # Ask the user whether they want to install extra casks
-read -p "Do you want to install extras? (Y/n) " answer
+echo -e "${purple}Do you want to install extras? (Y/n) ${no}"
+read -p "" answer
 if [ "$answer" != "N" ] && [ "$answer" != "n" ]; then
-    echo "Installing Extras... (Press C-c to cancel installing any of the extras you don't want)"
+    echo -e "${green}Installing Extras... ${no}"
     brew install --cask airbattery
     brew install --cask appdimmer
     brew install --cask dockdoor
@@ -132,27 +116,30 @@ if [ "$answer" != "N" ] && [ "$answer" != "n" ]; then
     brew install --cask yellowdot
 fi
 
-read -p "Choose a browser to install: (NONE/brave/zen/librewolf) " browser
-if [ "$browser" = "arc" ]; then
-    echo "Installing Brave..."
+echo -e "${purple}Choose a browser to install: (NONE/brave/zen/librewolf) ${no}"
+read -p "" browser
+if [ "$browser" = "brave" ]; then
+    echo -e "${green}Installing Brave...${no}"
     brew install --cask brave-browser
 elif [ "$browser" = "zen" ]; then
-    echo "Installing Zen..."
+    echo -e "${green}Installing Zen...${no}"
     brew install --cask zen-browser
 elif [ "$browser" = "librewolf" ]; then
-    echo "Installing librewolf..."
+    echo -e "${green}Installing librewolf...${no}"
     brew install --cask librewolf --no-quarantine
 fi
 
-read -p "Do you want to install Tor? (y/N) " install_tor
+echo -e "${purple}Do you want to install Tor? (y/N) ${no}"
+read -p "" install_tor
 if [ "$install_tor" = "Y" ] || [ "$install_tor" = "y" ]; then
-    echo "Installing Tor..."
+    echo -e "${green}Installing Tor...${no}"
     brew install --cask tor-browser
 fi
 
-read -p "Do you want to install additional fonts and symbols? (Used in my configuration) (Y/n) " add_fonts
+echo -e "${purple}Do you want to install additional fonts and symbols? (Used in my configuration) (Y/n) ${no}"
+read -p "" add_fonts
 if [ "$add_fonts" != "N" ] && [ "$add_fonts" != "n" ]; then
-    echo "Installing additional fonts... (Press C-c to cancel installing any of the fonts you don't want)"
+    echo -e "${green}Installing additional fonts... ${no}"
     brew install --cask font-jetbrains-mono-nerd-font
     brew install --cask font-sf-pro
     brew install --cask font-maple-mono-nf-cn
@@ -160,21 +147,24 @@ if [ "$add_fonts" != "N" ] && [ "$add_fonts" != "n" ]; then
     brew install --cask font-sketchybar-app-font
 fi
 
-read -p "Do you have external displays? (y/N) " has_external_displays
+echo -e "${purple}Do you have external displays? (y/N) ${no}"
+read -p "" has_external_displays
 if [ "$has_external_displays" = "Y" ] || [ "$has_external_displays" = "y" ]; then
-    echo "Installing BetterDisplay..."
+    echo -e "${green}Installing BetterDisplay...${no}"
     brew install --cask betterdisplay
 fi
 
-read -p "Do you have external mice? (y/N) " has_external_mice
+echo -e "${purple}Do you have external mice? (y/N) ${no}"
+read -p "" has_external_mice
 if [ "$has_external_mice" = "Y" ] || [ "$has_external_mice" = "y" ]; then
-    echo "Installing LinearMouse..."
+    echo -e "${green}Installing LinearMouse...${no}"
     brew install --cask linearmouse
 fi
 
-read -p "The following apps are not at all necessary, and for myself only. Still install? (y/N) " really
+echo -e "${red}The following apps are not at all necessary, and for myself only. Still install? (y/N) ${no}"
+read -p "" really
 if [ "$really" = "Y" ] || [ "$really" = "y" ]; then
-    echo "Installing... (Press C-c to cancel installing any of those apps)"
+    echo -e "${green}Installing... ${no}"
     brew install --cask dorico
     brew install --cask musescore
     brew install --cask qqmusic
@@ -184,7 +174,7 @@ if [ "$really" = "Y" ] || [ "$really" = "y" ]; then
 fi
 
 ## MacOS system settings
-echo "Writing MacOS system settings..."
+echo -e "${yellow}Writing MacOS system settings...${no}"
 
 # Hide menu bar
 defaults write NSGlobalDomain _HIHideMenuBar -bool true
@@ -214,7 +204,7 @@ defaults write com.apple.dock "mineffect" -string "scale"
 defaults write com.apple.finder "QuitMenuItem" -bool "true"
 
 # Show all file extensions
-defaults writedefaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 
 # Show hidden files
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
@@ -237,11 +227,12 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
 # Disable confirmation when closing unsaved windows (will autosave)
 defaults write NSGlobalDomain "NSCloseAlwaysConfirmsChanges" -bool "true"
 
-read -p "Disable quarantine and gatekeeper? (y/N) " quarantine
+echo -e "${red}Disable quarantine and gatekeeper? (y/N) ${no}"
+read -p "" quarantine
 if [ "$quarantine" = "Y" ] || [ "$quarantine" = "y" ]; then
     # Disable quarantine
     defaults write com.apple.LaunchServices "LSQuarantine" -bool "false"
-    echo "Type in your password to disable Gatekeeper:"
+    echo -e "${red}Type in your password to disable Gatekeeper:${no}"
     sudo spctl --master-disable
 fi
 
@@ -250,7 +241,7 @@ killall Dock
 
 # Clone dotfiles repository
 if [ ! -d "$HOME/dotfiles" ]; then
-    echo "Cloning dotfiles repository..."
+    echo -e "${green}Cloning dotfiles repository...${no}"
     git clone https://github.com/matt-dong-123/dotfiles.git "$HOME/dotfiles"
 fi
 
@@ -258,26 +249,28 @@ fi
 cd "$HOME/dotfiles" || exit
 
 # Stow dotfiles packages
-echo "Stowing dotfiles..."
-stow --ignore .DS_Store
-stow --ignore .git
-stow --ignore .gitignore
-stow --ignore .gitmodules
-stow --ignore README.md
-stow --ignore README.linkscape
-stow --ignore install.sh
-stow -t ~ .
+echo -e "${green}Stowing dotfiles...${no}"
+stow --ignore .DS_Store\
+     --ignore .git     \
+     --ignore .gitignore\
+     --ignore .gitmodules\
+     --ignore README.md\
+     --ignore README.linkscape\
+     --ignore install.sh\
+     -t ~ .
 
 # Set up hammerspoon config file
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "$HOME/.config/hammerspoon/init.lua"
 
-read -p "Set my wallpaper? (Y/n) " set_wallpaper
+echo -e "${purple}Set my wallpaper? (Y/n) ${no}"
+read -p "" set_wallpaper
 if [ "$set_wallpaper" != "N" ] && [ "$set_wallpaper" != "n" ]; then
     osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/wallpapers/wallpaper.png"'
 fi
 
 if [ "$browser" == "librewolf" ]; then
-    read -p "Use my librewolf config? (Y/n) " textfox
+    echo -e "${purple}Use my librewolf config? (Y/n) ${no}"
+    read -p "" textfox
     if [ "$textfox" != "N" ] && [ "$textfox" != "n" ]; then
         cd librewolf_config || exit
         chmod +x install.sh
@@ -285,16 +278,20 @@ if [ "$browser" == "librewolf" ]; then
     fi
 fi
 
+if [ "$browser" == "zen" ]; then
+    echo -e "${yellow} Set up zen config by yourself by pasting zen_config into your profile/chrome directory ${no}"
+fi
 # Set up one-thing config file
 if [ ! -f "$HOME/one-thing/one-thing.txt" ]; then
     mkdir -p "$HOME/one-thing"
     touch "$HOME/one-thing/one-thing.txt"
 fi
 
-echo "Setup complete!"
+echo -e "${green}Setup complete!${no}"
 
-read -p "Reboot for some settings to take effect? (Y/n) " reboot
+echo -e "${purple}Reboot for some settings to take effect? (Y/n) ${no}"
+read -p "" reboot
 if [ "$reboot" != "N" ] && [ "$reboot" != "n" ]; then
-    echo "Rebooting..."
+    echo -e "${red}Rebooting...${no}"
     sudo reboot
 fi
