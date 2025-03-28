acdc7206ca8a8e8e6e9518eee69abf837e07556a
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-28 22:28:50 +0800

    updated blink.cmp config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,19 +1,56 @@
 return {
     'saghen/blink.cmp',
-    -- OPTIONAL:
-    dependencies = 'rafamadriz/friendly-snippets',
-    version = '*',
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+    },
+    version = '1.*',
     opts = {
         keymap = { preset = 'default' },
         appearance = {
-            use_nvim_cmp_as_default = false,
             nerd_font_variant = 'mono',
         },
-        signature = { enabled = true },
-    },
-    sources = {
         completion = {
-            enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+            menu = {
+                border = 'rounded',
+            },
+            ghost_text = {
+                enabled = true,
+            },
+            documentation = {
+                auto_show = true,
+            },
         },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'cmdline' },
+        },
+        signature = {
+            enabled = true,
+        },
+        cmdline = {
+            enabled = true,
+            keymap = { preset = 'cmdline' },
+            sources = function()
+                local type = vim.fn.getcmdtype()
+                if type == '/' or type == '?' then
+                    return { 'buffer' }
+                end
+                if type == ':' or type == '@' then
+                    return { 'cmdline' }
+                end
+                return {}
+            end,
+            completion = {
+                list = {
+                    selection = {
+                        preselect = true,
+                        auto_insert = true,
+                    },
+                },
+                menu = { auto_show = true },
+                ghost_text = { enabled = true },
+            },
+        },
+        fuzzy = { implementation = 'prefer_rust_with_warning' },
     },
+    opts_extend = { 'sources.default' },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -31,9 +31,7 @@ return {
                     )
                 end
 
-                -- Jump to the definition of the word under your cursor.
-                --  This is where a variable was first declared, or where a function is defined, etc.
-                --  To jump back, press <C-t>.
+                -- Jump to the definition of the word under your cursor. (Press C-t to go back)
                 map(
                     'gd',
                     require('fzf-lua').lsp_definitions,
@@ -48,32 +46,27 @@ return {
                 )
 
                 -- Jump to the implementation of the word under your cursor.
-                --  Useful when your language has ways of declaring types without an actual implementation.
                 map(
                     'gI',
                     require('fzf-lua').lsp_implementations,
                     '[G]oto [I]mplementation'
                 )
 
-                -- Jump to the type of the word under your cursor.
-                --  Useful when you're not sure what type a variable is and you want to see
-                --  the definition of its *type*, not where it was *defined*.
+                -- Jump to the *type* of the word under your cursor.
                 map(
                     '<leader>D',
                     require('fzf-lua').lsp_typedefs,
                     'Type [D]efinition'
                 )
 
-                -- Fuzzy find all the symbols in your current document.
-                --  Symbols are things like variables, functions, types, etc.
+                -- Fuzzy find all the symbols (variables, functions, types, etc.) in your current document.
                 map(
                     '<leader>ds',
                     require('fzf-lua').lsp_document_symbols,
                     '[D]ocument [S]ymbols'
                 )
 
-                -- Fuzzy find all the symbols in your current workspace.
-                --  Similar to document symbols, except searches over your entire project.
+                -- Fuzzy find all the symbols in your current project.
                 map(
                     '<leader>ws',
                     require('fzf-lua').lsp_live_workspace_symbols,
@@ -81,7 +74,6 @@ return {
                 )
 
                 -- Rename the variable under your cursor.
-                --  Most Language Servers support renaming across files, etc.
                 map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
                 -- Execute a code action, usually your cursor needs to be on top of an error
@@ -97,10 +89,7 @@ return {
                 --  For example, in C this would take you to the header.
                 map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-                -- The following two autocommands are used to highlight references of the
-                -- word under your cursor when your cursor rests there for a little while.
-                --    See `:help CursorHold` for information about when this is executed
-                --
+                -- Highlight references of the word under your cursor when your cursor rests there for a little while.
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
                 if
@@ -146,10 +135,8 @@ return {
                     })
                 end
 
-                -- The following code creates a keymap to toggle inlay hints in your
-                -- code, if the language server you are using supports them
-                --
-                -- This may be unwanted, since they displace some of your code
+                -- The following code creates a keymap to toggle inlay hints in your code
+                -- WARN: This may be unwanted, since they displace some of your code
                 if
                     client
                     and client.supports_method(
@@ -182,57 +169,30 @@ return {
         --  By default, Neovim doesn't support everything that is in the LSP specification.
         --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
         --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend(
-            'force',
-            capabilities,
-            require('blink.cmp').get_lsp_capabilities()
-        )
+        local original_capabilities =
+            vim.lsp.protocol.make_client_capabilities()
+        local capabilities =
+            require('blink.cmp').get_lsp_capabilities(original_capabilities)
 
         -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
-        --  Add any additional override configuration in the following tables. Available keys are:
-        --  - cmd (table): Override the default command used to start the server
-        --  - filetypes (table): Override the default list of associated filetypes for the server
-        --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-        --  - settings (table): Override the default settings passed when initializing the server.
-        --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+        --  cmd (table): Override the default command used to start the server
+        --  filetypes (table): Override the default list of associated filetypes for the server
+        --  capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+        --  settings (table): Override the default settings passed when initializing the server.
+        --  For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
             clangd = {},
             gopls = {},
             pyright = {},
-            lua_ls = {
-                -- cmd = { ... },
-                -- filetypes = { ... },
-                -- capabilities = {},
-                settings = {
-                    Lua = {
-                        completion = {
-                            callSnippet = 'Replace',
-                        },
-                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        -- diagnostics = { disable = { 'missing-fields' } },
-                    },
-                },
-            },
+            lua_ls = {},
             -- rust_analyzer = {},
             -- ts_ls = {},
         }
 
-        -- Ensure the servers and tools above are installed
+        -- WARN: Ensure the servers and tools above are installed
         --
         -- To check the current status of installed tools and/or manually install
-        -- other tools, you can run
-        --    :Mason
-        --
-        -- You can press `g?` for help in this menu.
-        --
-        -- `mason` had to be setup earlier: to configure its options see the
-        -- `dependencies` table for `nvim-lspconfig` above.
-        --
-        -- You can add other tools here that you want Mason to install
-        -- for you, so that they are available from within Neovim.
+        -- other tools, you can run `:Mason`
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
             'stylua', -- Used to format Lua code
