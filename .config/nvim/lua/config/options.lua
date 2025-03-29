4de096c185998743bb8c8286d13a59ed110b6b15
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-30 04:37:23 +0800

    formatted options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,6 +4,9 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 
+-- show hidden files
+opt.hidden = true
+
 -- enable mouse
 opt.mouse = 'a'
 
@@ -78,7 +81,10 @@ opt.concealcursor = 'niv'
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    group = vim.api.nvim_create_augroup(
+        'kickstart-highlight-yank',
+        { clear = true }
+    ),
     callback = function()
         vim.highlight.on_yank()
     end,
