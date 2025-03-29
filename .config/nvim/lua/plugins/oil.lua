4828163a6e3ac87c363e53ad3a7f04ba645e5de4
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-30 04:37:43 +0800

    show hidden files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,13 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+        view_options = {
+            show_hidden = true,
+            is_hidden_file = function(name, bufnr)
+                return name ~= '..' and vim.startswith(name, '.')
+            end,
+        },
+    },
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     lazy = false,
     default_file_explorer = true,
