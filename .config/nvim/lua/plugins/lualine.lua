dc725b55a5c54bdc27bbc6a6ffd8d4e662151c93
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-30 03:27:48 +0800

    updated lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
         options = {
-            icons_enabled = true,
+            icons_enabled = vim.g.have_nerd_font,
             theme = 'auto',
             component_separators = { left = '', right = '' },
             section_separators = { left = '', right = '' },
