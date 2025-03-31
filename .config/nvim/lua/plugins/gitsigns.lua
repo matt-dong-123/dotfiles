d92b0d94d941317714e3fa9b9aff8148cc210615
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-03-31 23:31:01 +0800

    optimizing gitsigns config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,19 +1,6 @@
 return {
     'lewis6991/gitsigns.nvim',
-    opts = {
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
-    },
-    keys = {
-        {
-            '<leader>gp',
-            ':Gitsigns preview_hunk<CR>',
-            desc = '[G]itsigns [P]review',
-        },
-    },
+    event = 'BufReadPost',
+    opts = {},
+    vim.keymap.set('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>', {}),
 }
