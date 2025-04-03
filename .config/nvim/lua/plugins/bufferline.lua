2048505216aaf1bd9b86a11fde39037252df5803
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-03 21:54:19 +0800

    added bufferline picker

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,4 +2,11 @@ return {
     'akinsho/bufferline.nvim',
     event = 'BufReadPre',
     opts = {},
+    keys = {
+        {
+            'gb',
+            '<cmd>BufferLinePick<cr>',
+            desc = 'Pick buffer',
+        },
+    },
 }
