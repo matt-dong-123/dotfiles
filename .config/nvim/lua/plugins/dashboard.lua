683d407fa9f9a1dab3b40a3f83a16195c1f695d0
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-04 12:50:23 +0800

    restructured snacks

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,34 @@
+return {
+    'folke/snacks.nvim',
+    lazy = false,
+    opts = {
+        dashboard = {
+            sections = {
+                { section = 'header' },
+                { section = 'session' },
+                {
+                    icon = ' ',
+                    title = 'Keymaps',
+                    section = 'keys',
+                    indent = 2,
+                    padding = 1,
+                },
+                {
+                    icon = ' ',
+                    title = 'Recent Files',
+                    section = 'recent_files',
+                    indent = 2,
+                    padding = 1,
+                },
+                {
+                    icon = ' ',
+                    title = 'Projects',
+                    section = 'projects',
+                    indent = 2,
+                    padding = 1,
+                },
+                { section = 'startup' },
+            },
+        },
+    },
+}
