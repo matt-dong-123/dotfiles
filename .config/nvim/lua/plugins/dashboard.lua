a8a4746ec3f9a175c585796c37a71a6a39747cd0
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-05 22:13:09 +0800

    more PIPES

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -9,7 +9,7 @@ return {
                 {
                     pane = 2,
                     section = 'terminal',
-                    cmd = 'pipes.sh',
+                    cmd = 'pipes.sh -t3 -c1 -c2 -c3 -c4 -c5 -c6 -c7',
                     height = 10,
                     padding = 1,
                 },
