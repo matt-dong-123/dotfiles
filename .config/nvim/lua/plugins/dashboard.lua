699bfbdbae6cd35cbb5291491688957f18de1528
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-09 23:49:32 +0800

    added notes section to nvim, removed `new file`

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -3,6 +3,54 @@ return {
     lazy = false,
     opts = {
         dashboard = {
+            preset = {
+                keys = {
+                    {
+                        icon = ' ',
+                        key = 'f',
+                        desc = 'Find File',
+                        action = ":lua Snacks.dashboard.pick('files')",
+                    },
+                    {
+                        icon = ' ',
+                        key = 'g',
+                        desc = 'Find Text',
+                        action = ":lua Snacks.dashboard.pick('live_grep')",
+                    },
+                    {
+                        icon = ' ',
+                        key = 'r',
+                        desc = 'Recent Files',
+                        action = ":lua Snacks.dashboard.pick('oldfiles')",
+                    },
+                    {
+                        icon = ' ',
+                        key = 'c',
+                        desc = 'Config',
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                    },
+                    {
+                        icon = ' ',
+                        key = 'n',
+                        desc = 'Notes',
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.expand('$HOME/Notes')})",
+                    },
+                    {
+                        icon = ' ',
+                        key = 's',
+                        desc = 'Restore Session',
+                        section = 'session',
+                    },
+                    {
+                        icon = '󰒲 ',
+                        key = 'L',
+                        desc = 'Lazy',
+                        action = ':Lazy',
+                        enabled = package.loaded.lazy ~= nil,
+                    },
+                    { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+                },
+            },
             sections = {
                 { section = 'header' },
                 { section = 'keys', gap = 1, padding = 1 },
