ceeff67fda44b05077311b816d6e53a111dc73d7
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-11 20:47:35 +0800

    grind grind grind

---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,10 @@
+return {
+    'kawre/leetcode.nvim',
+    version = '*',
+    dependencies = {
+        'ibhagwan/fzf-lua',
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+    },
+    opts = {},
+}
