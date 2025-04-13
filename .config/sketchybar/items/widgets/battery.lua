5b79d2d5212d522adc3362d8ee5a726051797327
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-13 09:39:56 +0800

    removed popup (doesn't work for me)

--- .config/sketchybar/items/widgets/battery.lua
+++ .config/sketchybar/items/widgets/battery.lua
@@ -15,20 +15,6 @@ local battery = sbar.add("item", "widgets.battery", {
 	popup = { align = "center" },
 })
 
-local remaining_time = sbar.add("item", {
-	position = "popup." .. battery.name,
-	icon = {
-		string = "Time remaining:",
-		width = 100,
-		align = "left",
-	},
-	label = {
-		string = "??:??h",
-		width = 100,
-		align = "right",
-	},
-})
-
 battery:subscribe({ "routine", "power_source_change", "system_woke" }, function()
 	sbar.exec("pmset -g batt", function(batt_info)
 		local icon = "!"
@@ -78,19 +64,6 @@ battery:subscribe({ "routine", "power_source_change", "system_woke" }, function(
 			},
 		})
 	end)
-end)
-
-battery:subscribe("mouse.clicked", function()
-	local drawing = battery:query().popup.drawing
-	battery:set({ popup = { drawing = "toggle" } })
-
-	if drawing == "off" then
-		sbar.exec("pmset -g batt", function(batt_info)
-			local found, _, remaining = batt_info:find(" (%d+:%d+) remaining")
-			local label = found and remaining .. "h" or "No estimate"
-			remaining_time:set({ label = label })
-		end)
-	end
 end)
 
 sbar.add("bracket", "widgets.battery.bracket", { battery.name }, {
