5fc13ca698389c23ed6ed31dd983a77dd0265fe0
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-15 21:43:48 +0800

    replaced lazygit.nvim with toggleterm solution

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -17,17 +17,24 @@ return {
         {
             '<leader>tf',
             '<cmd>ToggleTerm direction=float<cr>',
-            desc = 'ToggleTerm float',
+            desc = '[T]erminal [F]loat',
         },
         {
             '<leader>th',
             '<cmd>ToggleTerm size=10 direction=horizontal<cr>',
-            desc = 'ToggleTerm horizontal split',
+            desc = '[T]erminal [H]orizontal split',
         },
         {
             '<leader>tv',
             '<cmd>ToggleTerm size=80 direction=vertical<cr>',
-            desc = 'ToggleTerm vertical split',
+            desc = '[T]erminal [V]ertical split',
+        },
+        {
+            '<leader>lg',
+            [[
+                <cmd>lua require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true }):toggle()<CR>'
+            ]],
+            desc = '[L]azy[G]it',
         },
     },
 }
