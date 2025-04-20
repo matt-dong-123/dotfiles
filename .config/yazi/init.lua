defb5ffb864e03d9b25c4744c9d7a46d4dc0d02c
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-04-20 21:10:07 +0800

    i need a status line for yazi, so bye bye yaziline

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,10 +1,64 @@
-require("yaziline"):setup({
-	color = "#7aa2f7",
-	separator_style = "liney",
-})
+-- require("yaziline"):setup({
+-- 	color = "#7aa2f7",
+-- 	separator_style = "liney",
+-- 	select_symbol = "",
+-- 	yank_symbol = "󰆐",
+-- })
 
 require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
 require("git"):setup()
+
+local tokyo_night_theme = require("yatline-tokyo-night"):setup("night")
+require("yatline"):setup({
+	theme = tokyo_night_theme,
+
+	-- Remove powerline separators
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_path" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
