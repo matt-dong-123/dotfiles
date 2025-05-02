7f7d699aa0e2f0879d5f75a17c9f54c85bbd92a4
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-02 17:04:13 +0800

    new autocmd to quit with q in some filetypes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,25 @@
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    desc = 'Highlight when yanking (copying) text',
     callback = function()
         vim.highlight.on_yank()
+    end,
+})
+
+-- Quit with q in certain filetypes
+vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('close-with-q', { clear = true }),
+    desc = 'Close with <q>',
+    pattern = {
+        'git',
+        'help',
+        'man',
+        'qf',
+        'query',
+        'scratch',
+    },
+    callback = function(args)
+        vim.keymap.set('n', 'q', '<cmd>quit<cr>', { buffer = args.buf })
     end,
 })
