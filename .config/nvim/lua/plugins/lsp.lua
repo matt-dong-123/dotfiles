4cae268b4da60652fe967079dde37bd5f97fe28b
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-07 18:31:03 +0800

    breaking changes, removed automatic_installation

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -208,7 +208,6 @@ return {
 
         require('mason-lspconfig').setup {
             ensure_installed = {},
-            automatic_installation = false,
             handlers = {
                 function(server_name)
                     local server = servers[server_name] or {}
