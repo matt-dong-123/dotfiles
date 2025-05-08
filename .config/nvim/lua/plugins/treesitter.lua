8fc9d0bf7ae5a3d7d54aff77fb2a39ead8c9fe28
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-08 17:30:30 +0800

    some tweaks

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,10 +16,10 @@ return {
             highlight = {
                 enable = true,
                 -- Disable for large files
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 Kb
                     local ok, stats =
-                        pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
                         return true
                     end
