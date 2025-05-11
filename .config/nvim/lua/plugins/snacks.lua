cd56b1ac67e6535d7802c6702a67a03f958721a2
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-11 18:29:15 +0800

    lazy events

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,5 +1,6 @@
 return {
     'folke/snacks.nvim',
+    event = 'VeryLazy',
     priority = 1000,
     lazy = false,
     opts = {
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    event = { 'BufReadPost', 'BufNewFile' },
     build = ':TSUpdate',
     config = function()
         ---@diagnostic disable-next-line: missing-fields
