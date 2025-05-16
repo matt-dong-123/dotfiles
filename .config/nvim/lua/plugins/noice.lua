a7669fb9a8e0f81d8cea69e9c6269948c8aef29a
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-16 21:06:52 +0800

    misc

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,12 +2,6 @@ return {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
-        lsp = {
-            override = {
-                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                ['vim.lsp.util.stylize_mardown'] = true,
-            },
-        },
         presets = {
             bottom_search = true,
             command_palette = true,
