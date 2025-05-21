daa385652745602445d81d6727785d48785a655c
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-21 22:12:47 +0800

    tweaked settings

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,11 +2,12 @@ local colors = require("colors")
 
 sbar.bar({
 	topmost = "window",
+	shadow = true,
 	height = 40,
 	color = colors.bar.bg,
 	corner_radius = 15,
-	padding_right = 2,
-	padding_left = 2,
+	padding_right = 10,
+	padding_left = 10,
 	position = "top",
 	margin = 5,
 })
