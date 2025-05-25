e3ef7cc0b0bff6032367808274c517b9b1cf0bee
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-25 11:15:29 +0800

    keep ts up to date

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,22 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     event = { 'BufReadPost', 'BufNewFile' },
+    branch = 'main',
     build = ':TSUpdate',
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = {
-                'c',
-                'lua',
-                'vim',
-                'vimdoc',
-                'query',
-                'markdown',
-                'markdown_inline',
-            },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                -- Disable for large files
-                disable = function(_, buf)
-                    local max_filesize = 100 * 1024 -- 100 Kb
-                    local ok, stats =
-                        pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
-            indent = { enable = true },
-        }
-    end,
+    opts = {
+        auto_install = true,
+        highlight = {
+            enable = true,
+            -- Disable for large files
+            disable = function(_, buf)
+                local max_filesize = 100 * 1024 -- 100 Kb
+                local ok, stats =
+                    pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+        },
+        indent = { enable = true },
+    },
 }
