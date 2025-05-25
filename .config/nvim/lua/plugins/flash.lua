d943d7e3415dc36a7af6be770edfa5565cdb3690
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-05-25 17:22:02 +0800

    added all keys back

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -41,5 +41,13 @@ return {
             end,
             desc = 'Treesitter Search',
         },
+        {
+            '<c-s>',
+            mode = { 'c' },
+            function()
+                require('flash').toggle()
+            end,
+            desc = 'Toggle Flash Search',
+        },
     },
 }
