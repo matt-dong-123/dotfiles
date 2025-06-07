fd61b788f7ca49ad4be3a916f799ca8a9cdb5b19
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-06-07 13:15:14 +0800

    smart splits (TODO: does this work?)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local workspace_switcher =
     wezterm.plugin.require 'https://github.com/MLFlexer/smart_workspace_switcher.wezterm'
+local smart_splits =
+    wezterm.plugin.require 'https://github.com/mrjones2014/smart-splits.nvim'
 workspace_switcher.apply_to_config(config)
+smart_splits.apply_to_config(config)
 
 config = {
     -- APPEARANCE
