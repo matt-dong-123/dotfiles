4ee7189bcc8b9e96694a4e36aba07f5644165396
matt-dong-123 <bruckheim@proton.me>
2025-06-23 10:59:48 +0800

    don't need this either

--- .config/wezterm/behavior.lua
+++ .config/wezterm/behavior.lua
@@ -3,7 +3,7 @@ local M = {}
 M.apply_to_config = function(config)
     config.max_fps = 120
     config.window_close_confirmation = 'NeverPrompt'
-    config.window_decorations = 'RESIZE | MACOS_FORCE_ENABLE_SHADOW'
+    config.window_decorations = 'RESIZE'
     config.tab_max_width = 100
     config.show_new_tab_button_in_tab_bar = false
 end
