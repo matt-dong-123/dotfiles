c5c9f0b38c571e68dc27dce4518b5174b3d12aab
matt-dong-123 <bruckheim@proton.me>
2025-06-27 17:26:41 +0200

    wow borders are cool now!

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,11 +2,7 @@ local colors = require("colors")
 
 sbar.bar({
 	topmost = "window",
-	height = 40,
+	height = 38,
 	color = colors.bar.bg,
-	corner_radius = 15,
-	padding_right = 10,
-	padding_left = 10,
 	position = "top",
-	margin = 5,
 })
