8413267ab0a143de7f1020a18bed5dcecc239b51
matt-dong-123 <bruckheim@proton.me>
2025-07-19 16:38:09 +0800

    i love the feeling of not knowing what browsers i should use

--- .config/hammerspoon/app_hotkeys.lua
+++ .config/hammerspoon/app_hotkeys.lua
@@ -1,7 +1,7 @@
 local hypr = { "ctrl", "shift", "cmd" }
 
 local hotkeys = {
-	a = "Zen",
+	a = "Floorp",
 	d = "Dorico 5",
 	f = "Finder",
 	l = "Logic Pro",
--- install.sh
+++ install.sh
@@ -89,12 +89,18 @@ brew install --cask alt-tab
 brew install --cask iina
 brew install --cask input-source-pro
 brew install --cask only-switch
-brew install --cask mullvad-browser
-brew install --cask zen
+brew install --cask floorp
+
+echo -e "${purple}Do you want to install Mullvad? (y/N) ${no_color}"
+read -p "" install_mullvad
+if [ "$install_mullvad" = "Y" ] || [ "$install_mullvad" = "y" ]; then
+    echo -e "${green}Installing Mullvad...${no_color}"
+    brew install --cask mullvad-browser
+fi
 
 echo -e "${purple}Do you want to install Tor? (y/N) ${no_color}"
-read -p "" install_tor
-if [ "$install_tor" = "Y" ] || [ "$install_tor" = "y" ]; then
+read -p "" install_mullvad
+if [ "$install_mullvad" = "Y" ] || [ "$install_mullvad" = "y" ]; then
     echo -e "${green}Installing Tor...${no_color}"
     brew install --cask tor-browser
 fi
