74603cf3faa31dbbb5ec175ac0af7bab7def6e44
matt-dong-123 <bruckheim@proton.me>
2025-07-24 09:46:00 +0800

    update

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {
+        columns = {
+            'icon',
+            'permissions',
+            'mtime',
+        },
+    },
+    dependencies = {
+        'echasnovski/mini.icons',
+    },
+    lazy = false,
+}
