2c745cde2d562602de4b27c1d6e19223a8e7ded2
matt-dong-123 <bruckheim@proton.me>
2025-07-24 14:27:14 +0800

    refine hidden file rules

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,16 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, bufnr)
+                if name == '.DS_Store' then
+                    return true
+                end
+                return false
+            end,
+        },
+    },
     dependencies = {
         'echasnovski/mini.icons',
     },
