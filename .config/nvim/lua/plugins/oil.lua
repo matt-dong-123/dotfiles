a26f696db9ab30d70ef0b2e5de8bf6cea72642e0
matt-dong-123 <bruckheim@proton.me>
2025-07-26 15:55:29 +0800

    skip confirm for simple edits oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
     'stevearc/oil.nvim',
     opts = {
+        skip_confirm_for_simple_edits = true,
         view_options = {
             show_hidden = true,
             is_always_hidden = function(name, bufnr)
