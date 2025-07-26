3cd8e0e114b440c9ae10705fc9360b5a86578a45
matt-dong-123 <bruckheim@proton.me>
2025-07-26 21:35:14 +0800

    "make the dashboard as simple as possible!" "aye aye, captain!"

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -12,12 +12,6 @@ return {
                         action = ":lua Snacks.dashboard.pick('smart')",
                     },
                     {
-                        icon = ' ',
-                        key = 'g',
-                        desc = 'Find Text',
-                        action = ":lua Snacks.dashboard.pick('live_grep')",
-                    },
-                    {
                         icon = ' ',
                         key = 'r',
                         desc = 'Recent Files',
@@ -36,17 +30,17 @@ return {
                         action = ":lua Snacks.dashboard.pick('smart', {cwd = vim.fn.expand('$HOME/Notes')})",
                     },
                     {
-                        icon = ' ',
-                        key = 's',
-                        desc = 'Restore Session',
-                        section = 'session',
-                    },
-                    {
                         icon = '󰒲 ',
                         key = 'l',
                         desc = 'Lazy',
                         action = ':Lazy',
                         enabled = package.loaded.lazy ~= nil,
+                    },
+                    {
+                        icon = ' ',
+                        key = 'm',
+                        desc = 'Mason',
+                        action = ':Mason',
                     },
                     { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
                 },
@@ -54,17 +48,7 @@ return {
             sections = {
                 {
                     section = 'keys',
-                    icon = '󰌌 ',
-                    title = 'Keys',
-                    indent = 2,
                     gap = 1,
-                    padding = 1,
-                },
-                {
-                    icon = ' ',
-                    title = 'Recent Files',
-                    section = 'recent_files',
-                    indent = 2,
                     padding = 1,
                 },
                 { section = 'startup' },
