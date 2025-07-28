5ad7b8cc5afcc3e4c41f3d7d8bee29ba7dcf00bb
matt-dong-123 <bruckheim@proton.me>
2025-07-28 20:04:34 +0800

    found conflicts

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -28,25 +28,5 @@ return {
             map('smart-splits', 'move_cursor_right'),
             desc = 'Move to right window',
         },
-        {
-            '<A-h>',
-            map('smart-splits', 'resize_left'),
-            desc = 'Resize window left',
-        },
-        {
-            '<A-j>',
-            map('smart-splits', 'resize_down'),
-            desc = 'Resize window down',
-        },
-        {
-            '<A-k>',
-            map('smart-splits', 'resize_up'),
-            desc = 'Resize window up',
-        },
-        {
-            '<A-l>',
-            map('smart-splits', 'resize_right'),
-            desc = 'Resize window right',
-        },
     },
 }
