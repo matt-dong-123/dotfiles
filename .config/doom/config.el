e5590ae0652c82b871cfeebadb7415809643d11d
matt-dong-123 <bruckheim@proton.me>
2025-07-28 23:04:09 +0800

    close emacs without prompting

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -41,7 +41,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/notes/")
 
-
+(setq confirm-kill-emacs nil)
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
