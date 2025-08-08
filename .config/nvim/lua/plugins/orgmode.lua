a21a48a22f83bc6a072381a7a319cc1b48dcef52
matt-dong-123 <bruckheim@proton.me>
2025-08-08 20:49:41 +0800

    I USE FUCKING EMACS TO TAKE MY NOTES NOW

--- .config/nvim/lua/plugins/orgmode.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-orgmode/orgmode',
-    lazy = false,
-    ft = 'org',
-    opts = {
-        org_agenda_files = '~/Notes/**/*',
-        org_default_notes_file = '~/Notes/refile.org',
-    },
-}
