1e6d71064cd6016043065acd59ecd3be2ab6c998
matt-dong-123 <bruckheim@proton.me>
2025-08-12 14:33:45 +0800

    looks a lot more balanced now

--- .config/sketchybar/items/left/front_app.lua
+++ .config/sketchybar/items/left/front_app.lua
@@ -6,14 +6,14 @@ local front_app = sbar.add("item", "front_app", {
 	display = "active",
 	icon = {
 		font = "sketchybar-app-font:Regular:20.0",
-		color = colors.yellow,
+		color = colors.default,
 	},
 	label = {
 		font = {
 			family = settings.font.text,
 			size = 16.0,
 		},
-		color = colors.yellow,
+		color = colors.default,
 	},
 	updates = true,
 })
