8aabf149d25c33dce969b3dbee8c9e6dd7ae7044
matt-dong-123 <bruckheim@proton.me>
2025-08-20 10:03:13 +0800

    new line before main prompt??

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -35,7 +35,8 @@ prompt_char() {
 precmd() { LAST_CMD_EXIT=$? }
 
 # prompt
-PROMPT='${BLUE}%~${RESET} $(git_info)
+PROMPT='
+${BLUE}%~${RESET} $(git_info)
 $(prompt_char) '
 TRANSIENT_PROMPT_PROMPT=$PROMPT
 TRANSIENT_PROMPT_TRANSIENT_PROMPT="$(prompt_char) "
