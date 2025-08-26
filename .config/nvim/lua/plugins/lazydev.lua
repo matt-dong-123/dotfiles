cda2050ac11086acafcae09921dec5e75f84476c
matt-dong-123 <bruckheim@proton.me>
2025-08-26 12:54:20 +0800

    nvm wezterm types don't even work

--- .config/nvim/lua/plugins/lazydev.lua
+++ .config/nvim/lua/plugins/lazydev.lua
@@ -1,14 +1,10 @@
 return {
     'folke/lazydev.nvim',
-    dependencies = {
-        { 'DrKJeff16/wezterm-types', lazy = true },
-    },
     ft = 'lua',
     opts = {
         library = {
             { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
             { path = 'snacks.nvim', words = { 'Snacks' } },
-            { path = 'wezterm-types', words = { 'wezterm ' } },
         },
     },
 }
