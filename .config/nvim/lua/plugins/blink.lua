3559a2c5017fe9cdeae96f3931535c0b18fd4c72
matt-dong-123 <bruckheim@proton.me>
2025-08-30 23:44:54 +0800

    cleaned up blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,21 +1,14 @@
 return {
     'saghen/blink.cmp',
-    event = { 'InsertEnter', 'CmdlineEnter' },
     dependencies = {
         'rafamadriz/friendly-snippets',
+        'folke/lazydev.nvim',
     },
     version = '*',
     opts = {
-        keymap = { preset = 'default' },
-        appearance = {
-            nerd_font_variant = 'mono',
-        },
         completion = {
             menu = {
                 border = 'rounded',
-            },
-            ghost_text = {
-                enabled = true,
             },
         },
         sources = {
@@ -38,29 +31,9 @@ return {
         },
         cmdline = {
             enabled = true,
-            keymap = { preset = 'cmdline' },
-            sources = function()
-                local type = vim.fn.getcmdtype()
-                if type == '/' or type == '?' then
-                    return { 'buffer' }
-                end
-                if type == ':' or type == '@' then
-                    return { 'cmdline' }
-                end
-                return {}
-            end,
             completion = {
-                list = {
-                    selection = {
-                        preselect = true,
-                        auto_insert = true,
-                    },
-                },
                 menu = { auto_show = true },
-                ghost_text = { enabled = true },
             },
         },
-        fuzzy = { implementation = 'prefer_rust' },
     },
-    opts_extend = { 'sources.default' },
 }
