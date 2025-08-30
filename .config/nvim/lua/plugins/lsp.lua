c33a911a6ce2168128d09a3d083abfd0afba336a
matt-dong-123 <bruckheim@proton.me>
2025-08-30 23:50:22 +0800

    cleaned up picker config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,4 +13,58 @@ return {
             }
         end,
     },
+    {
+        'folke/snacks.nvim',
+        keys = {
+            {
+                '<leader>ld',
+                function()
+                    Snacks.picker.lsp_definitions()
+                end,
+                desc = 'Definition',
+            },
+            {
+                '<leader>lD',
+                function()
+                    Snacks.picker.lsp_declarations()
+                end,
+                desc = 'Declaration',
+            },
+            {
+                '<leader>lr',
+                function()
+                    Snacks.picker.lsp_references()
+                end,
+                desc = 'References',
+            },
+            {
+                '<leader>li',
+                function()
+                    Snacks.picker.lsp_implementations()
+                end,
+                desc = 'Implementations',
+            },
+            {
+                '<leader>lt',
+                function()
+                    Snacks.picker.lsp_type_definitions()
+                end,
+                desc = 'Type Definitions',
+            },
+            {
+                '<leader>ls',
+                function()
+                    Snacks.picker.lsp_symbols()
+                end,
+                desc = 'Document Symbols',
+            },
+            {
+                '<leader>lS',
+                function()
+                    Snacks.picker.lsp_workspace_symbols()
+                end,
+                desc = 'Workspace Symbols',
+            },
+        },
+    },
 }
