8696661a62f731c443012d0bc8d9a6ab4afb3502
matt-dong-123 <bruckheim@proton.me>
2025-08-31 22:06:56 +0800

    added this back

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
     version = false,
     config = function()
         require('mini.ai').setup()
+        require('mini.bracketed').setup()
         require('mini.icons').setup()
         require('mini.operators').setup()
         require('mini.pairs').setup {
