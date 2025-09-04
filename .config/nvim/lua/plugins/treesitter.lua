41c06bf882378661483d48e8bd1121bcdcd4eb9f
matt-dong-123 <bruckheim@proton.me>
2025-09-04 22:08:41 +0800

    feat: ✨ Reformatted

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -26,8 +26,7 @@ return {
             -- Disable for large files
             disable = function(_, buf)
                 local max_filesize = 100 * 1024 -- 100 Kb
-                local ok, stats =
-                    pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
                     return true
                 end
