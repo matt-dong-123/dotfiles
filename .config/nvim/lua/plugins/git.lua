a9969c1a452d3c2f7b88cf790ee3a352382ec630
matt-dong-123 <bruckheim@proton.me>
2025-09-04 19:16:54 +0800

    feat: ✨ Added keybindings for navigating between hunks

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -42,6 +42,23 @@ return {
     },
     {
         'lewis6991/gitsigns.nvim',
+        lazy = false,
+        keys = {
+            {
+                '[h',
+                function()
+                    package.loaded.gitsigns.nav_hunk 'prev'
+                end,
+                desc = 'Previous Hunk',
+            },
+            {
+                ']h',
+                function()
+                    package.loaded.gitsigns.nav_hunk 'next'
+                end,
+                desc = 'Next Hunk',
+            },
+        },
     },
     {
         'folke/snacks.nvim',
