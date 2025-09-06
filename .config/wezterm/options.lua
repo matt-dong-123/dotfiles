4bd4ba5ffe0621ff7dec32eb29b5190c127a9092
matt-dong-123 <bruckheim@proton.me>
2025-09-06 14:23:36 +0800

    feat: ✨ The ligatures and italics of JetBrains Mono aren't enough

--- .config/wezterm/options.lua
+++ .config/wezterm/options.lua
@@ -4,7 +4,7 @@ local M = {}
 
 M.apply_to_config = function(config)
     config.color_scheme = 'tokyonight_moon'
-    config.font = wezterm.font 'JetBrainsMono Nerd Font'
+    config.font = wezterm.font 'Maple Mono NF CN'
     config.use_fancy_tab_bar = false
     config.window_background_opacity = 0.75
     config.macos_window_background_blur = 100
