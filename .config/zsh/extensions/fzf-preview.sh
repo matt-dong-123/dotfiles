92f4de7072795298e42a529785f4c13cdb3f56ed
Matthew Dong <bruckheim@proton.me>
2025-09-12 18:43:52 +0800

    feat: ✨ Added custom preview script

---
+++ .config/zsh/extensions/fzf-preview.sh
@@ -0,0 +1,19 @@
+ #!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+  >&2 echo "usage: $0 FILENAME[:LINENO][:IGNORED]"
+  exit 1
+fi
+
+file=${1/#\~\//$HOME/}
+type=$(file --brief --dereference --mime -- "$file")
+
+if [ -d $file ]; then
+    eza --tree --color=always $file --icons | head -200
+elif [[ $type =~ image/ ]]; then
+    chafa -s 40 $file
+elif [[ $type =~ =binary ]]; then
+    file "$1"
+else
+    bat -n --color=always --line-range :500 $file
+fi
