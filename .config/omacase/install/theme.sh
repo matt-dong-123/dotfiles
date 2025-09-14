ca663d1a783c41ba57b7f06f525d7b30e3b2bc13
Matthew Dong <bruckheim@proton.me>
2025-09-14 17:14:23 +0800

    feat: ✨ Unified yazi status bar style

--- .config/omacase/install/theme.sh
+++ .config/omacase/install/theme.sh
@@ -5,6 +5,8 @@ mkdir -p ~/.config/omacase/current
 ln -snf ~/.config/omacase/themes/tokyo-night ~/.config/omacase/current/theme
 osascript -e 'tell application "System Events" to set picture of every desktop to "~/.config/omacase/themes/tokyo-night/backgrounds/default.png"'
 
+mkdir -p ~/.config/btop/themes ~/.config/lazygit
+
 # Set specific app links for current theme
 ln -snf ~/.config/omacase/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 ln -snf ~/.config/omacase/current/theme/wezterm.lua ~/.config/wezterm/theme.lua
