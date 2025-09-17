ec7d4f4a3d7c4f4df9e139072273a93082a79a89
Matthew Dong <bruckheim@proton.me>
2025-09-17 18:17:31 +0800

    feat: ✨ Whoops

--- .config/wezterm/options.lua
+++ .config/wezterm/options.lua
@@ -22,21 +22,6 @@ M.apply_to_config = function(config)
     config.window_decorations = 'RESIZE | MACOS_FORCE_ENABLE_SHADOW'
     config.tab_max_width = 100
     config.show_new_tab_button_in_tab_bar = false
-    wezterm.on('update-right-status', function(window, _)
-        local prefix = '  ' .. wezterm.mux.get_active_workspace() .. ' '
-        if window:leader_is_active() then
-            prefix = ' 󰠠 ' .. wezterm.mux.get_active_workspace() .. ' '
-            window:set_left_status(wezterm.format {
-                { Foreground = { Color = '#ff757f' } },
-                { Text = prefix },
-            })
-        else
-            window:set_left_status(wezterm.format {
-                { Foreground = { Color = '#4fd6be' } },
-                { Text = prefix },
-            })
-        end
-    end)
 end
 
 return M
