2c7f0358e1a01abb9437a13f34ac73dac80a6c3d
Matthew Dong <bruckheim@proton.me>
2025-09-20 11:13:01 +0800

    feat: ✨ Made MacOS more square

--- .config/wezterm/options.lua
+++ .config/wezterm/options.lua
@@ -13,7 +13,8 @@ M.apply_to_config = function(config)
     config.max_fps = 120
     config.prefer_egl = true
     config.window_close_confirmation = 'NeverPrompt'
-    config.window_decorations = 'RESIZE | MACOS_FORCE_ENABLE_SHADOW'
+    config.window_decorations =
+        'RESIZE | MACOS_FORCE_ENABLE_SHADOW | MACOS_FORCE_SQUARE_CORNERS'
     config.tab_max_width = 100
     config.show_new_tab_button_in_tab_bar = false
 end
