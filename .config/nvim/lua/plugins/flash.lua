ef51d661278826d69f5f3b25da6b895970e3605b
Matthew Dong <bruckheim@proton.me>
2025-09-20 09:46:30 +0800

    feat: ✨ Readded flash

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,42 @@
+return {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+        label = { style = 'inline' },
+        modes = { char = { enabled = false } },
+    },
+    keys = {
+        {
+            'ys',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').jump()
+            end,
+            desc = 'Flash',
+        },
+        {
+            'yS',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').treesitter()
+            end,
+            desc = 'Flash Treesitter',
+        },
+        {
+            'r',
+            mode = 'o',
+            function()
+                require('flash').remote()
+            end,
+            desc = 'Remote Flash',
+        },
+        {
+            'R',
+            mode = { 'o', 'x' },
+            function()
+                require('flash').treesitter_search()
+            end,
+            desc = 'Treesitter Search',
+        },
+    },
+}
