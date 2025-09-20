b1974140e6180ffaa64147f4f14c68580c4ec858
Matthew Dong <bruckheim@proton.me>
2025-09-20 09:19:39 +0800

    feat: ✨ Replaced yazi.nvim with oil.nvim (again)

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                if name == '.DS_Store' then
+                    return true
+                end
+                return false
+            end,
+        },
+    },
+    dependencies = {
+        'nvim-mini/mini.nvim',
+    },
+    keys = {
+        {
+            '<leader>e',
+            '<cmd>Oil --preview<CR>',
+            desc = 'Open oil with preview',
+        },
+    },
+    lazy = false,
+}
