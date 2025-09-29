abc35e5a02a598576fad62210fb916a77bf125eb
Matthew Dong <bruckheim@proton.me>
2025-09-29 19:09:32 +0800

    fix(delta): Directly source omacase

--- .config/omacase/install/theme.sh
+++ .config/omacase/install/theme.sh
@@ -15,6 +15,5 @@ ln -snf ~/.config/omacase/current/theme/lazygit.yml ~/.config/lazygit/config.yml
 ln -snf ~/.config/omacase/current/theme/btop.theme ~/.config/btop/themes/current.theme
 ln -snf ~/.config/omacase/current/theme/bat ~/.config/bat/config
 ln -snf ~/.config/omacase/current/theme/yazi.toml ~/.config/yazi/theme.toml
-ln -snf ~/.config/omacase/current/theme/delta-colors ~/.config/git/delta-colors
 
 brew services restart borders sketchybar
