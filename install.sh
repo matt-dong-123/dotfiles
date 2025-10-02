82b1d94c54f4a5e88830402bca142a345fe20037
Matthew Dong <bruckheim@proton.me>
2025-10-02 17:49:49 +0800

    refactor(install): removed install.sh in .config/omacase

--- install.sh
+++ install.sh
@@ -92,6 +92,21 @@ fi
 log "${green}Stowing dotfiles...${no_color}"
 cd ~/dotfiles && stow .
 
-~/.config/omacase/install/theme.sh
+# Set initial theme
+mkdir -p ~/.config/omacase/current
+ln -snf ~/.config/omacase/themes/tokyo-night-moon ~/.config/omacase/current/theme
+osascript -e 'tell application "System Events" to set picture of every desktop to "~/.config/omacase/themes/tokyo-night/backgrounds/default.png"'
+
+mkdir -p ~/.config/btop/themes ~/.config/lazygit
+
+# Set specific app links for current theme
+ln -snf ~/.config/omacase/current/theme/neovim.lua ~/.config/nvim/lua/config/colorscheme.lua
+ln -snf ~/.config/omacase/current/theme/wezterm.lua ~/.config/wezterm/theme.lua
+ln -snf ~/.config/omacase/current/theme/sketchybar.lua ~/.config/sketchybar/colors.lua
+ln -snf ~/.config/omacase/current/theme/btop.theme ~/.config/btop/themes/current.theme
+ln -snf ~/.config/omacase/current/theme/bat ~/.config/bat/config
+ln -snf ~/.config/omacase/current/theme/yazi.toml ~/.config/yazi/theme.toml
+
+brew services restart borders sketchybar
 
 log "${green}Setup complete!${no_color}"
