c1958866073caa91d6e8abdb6f51d7c243f5252b
Matthew Dong <bruckheim@proton.me>
2025-10-04 20:25:57 +0800

    fix(install): Point background to correct path

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ ya pkg install
 # Set initial theme
 mkdir -p ~/.config/omacase/current
 ln -snf ~/.config/omacase/themes/tokyo-night-moon ~/.config/omacase/current/theme
-osascript -e "tell application \"System Events\" to set picture of every desktop to POSIX file \"$HOME/.config/omacase/themes/tokyo-night/backgrounds/default.png\""
+osascript -e "tell application \"System Events\" to set picture of every desktop to POSIX file \"$HOME/.config/omacase/themes/tokyo-night-moon/backgrounds/default.png\""
 
 mkdir -p ~/.config/btop/themes
 
