539ee97ea5c4c057323c89dcf659f35fdf7952b4
Matthew Dong <bruckheim@proton.me>
2025-10-09 22:45:18 +0800

    feat(nvim): added lazy loading to grug-far.nvim

--- .config/nvim/lua/plugins/grug-far.lua
+++ .config/nvim/lua/plugins/grug-far.lua
@@ -1,4 +1,14 @@
 return {
     'MagicDuck/grug-far.nvim',
     opts = {},
+    cmd = { 'GrugFar', 'GrugFarWithin' },
+    keys = {
+        {
+            '<leader>fr',
+            function()
+                require('grug-far').open()
+            end,
+            desc = 'Replace',
+        },
+    },
 }
