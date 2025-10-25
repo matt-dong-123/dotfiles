b25244e0e126283885a66d025e26eb9fb7b05c30
Matthew Dong <bruckheim@proton.me>
2025-10-26 01:35:23 +0800

    feat(nvim): Added octo.nvim

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,11 @@
+return {
+    'pwntester/octo.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'folke/snacks.nvim',
+        'nvim-mini/mini.nvim',
+    },
+    opts = {
+        picker = 'snacks',
+    },
+}
