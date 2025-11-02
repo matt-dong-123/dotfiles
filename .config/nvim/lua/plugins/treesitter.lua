a368f651040224698a1ad70dffd559255a1c9cfe
Matthew Dong <bruckheim@proton.me>
2025-11-02 19:38:52 +0800

    feat(nvim): remove markview

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
     build = ':TSUpdate',
-    cond = function()
-        return vim.bo.filetype ~= 'markdown'
-    end,
     opts = {
         ensure_installed = {
             'cpp',
