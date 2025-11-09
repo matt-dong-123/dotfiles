a7a735cb48e4de818612191b30a77e94b947d395
Matthew Dong <bruckheim@proton.me>
2025-11-09 11:49:31 +0800

    feat(zsh): Compile entire $ZDOTDIR

--- .config/zsh/.zshrc.d/zgenom.zsh
+++ .config/zsh/.zshrc.d/zgenom.zsh
@@ -2,33 +2,33 @@
 ZGENOM_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zgenom/zgenom.git"
 
 if [ ! -d "${ZGENOM_HOME}" ]; then
-	mkdir -p "$(dirname "${ZGENOM_HOME}")"
-	git clone https://github.com/jandamm/zgenom "${ZGENOM_HOME}"
+    mkdir -p "$(dirname "${ZGENOM_HOME}")"
+    git clone https://github.com/jandamm/zgenom "${ZGENOM_HOME}"
 fi
 
 source "${ZGENOM_HOME}/zgenom.zsh"
 
 zgenom autoupdate
 if ! zgenom saved; then
-	zgenom load zdharma-continuum/fast-syntax-highlighting
-	zgenom load zsh-users/zsh-autosuggestions
-	zgenom load zsh-users/zsh-completions src
+    zgenom load zdharma-continuum/fast-syntax-highlighting
+    zgenom load zsh-users/zsh-autosuggestions
+    zgenom load zsh-users/zsh-completions src
 
-	zgenom load olets/zsh-transient-prompt
+    zgenom load olets/zsh-transient-prompt
 
-	zgenom ohmyzsh plugins/sudo
+    zgenom ohmyzsh plugins/sudo
 
-	zgenom load jeffreytse/zsh-vi-mode
+    zgenom load jeffreytse/zsh-vi-mode
 
-	zgenom save
+    zgenom save
 
-	zgenom compile "$ZDOTDIR/.zshrc"
+    zgenom compile "$ZDOTDIR"
 fi
 
 function zvm_after_init() {
-	source <(fzf --zsh)
-	zgenom load junegunn/fzf-git.sh
-	zgenom load Aloxaf/fzf-tab
+    source <(fzf --zsh)
+    zgenom load junegunn/fzf-git.sh
+    zgenom load Aloxaf/fzf-tab
 
-	zgenom load hlissner/zsh-autopair autopair.zsh
+    zgenom load hlissner/zsh-autopair autopair.zsh
 }
