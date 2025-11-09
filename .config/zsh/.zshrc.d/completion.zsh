bb959a465698469a7d4215139c187c8e78f81b40
Matthew Dong <bruckheim@proton.me>
2025-11-09 11:43:58 +0800

    fix(zsh): remove manual compinit as zgenom handles it

--- .config/zsh/.zshrc.d/completion.zsh
+++ .config/zsh/.zshrc.d/completion.zsh
@@ -3,6 +3,5 @@ zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
 zstyle ':fzf-tab:complete:*' fzf-flags \
-	--no-height \
-	--preview=''
-autoload -U compinit && compinit
+    --no-height \
+    --preview=''
