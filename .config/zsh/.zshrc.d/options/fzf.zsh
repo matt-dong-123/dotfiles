6fb0e36fd870aa7f6a97a4cae93ff70113bfca9b
Matthew Dong <bruckheim@proton.me>
2025-11-14 23:48:24 +0800

    perf(fzf): remove unuseful lines

--- .config/zsh/.zshrc.d/options/fzf.zsh
+++ .config/zsh/.zshrc.d/options/fzf.zsh
@@ -6,13 +6,7 @@ export FZF_CTRL_R_OPTS="--preview ''"
 export FZF_GIT_CAT="bat -n --color=always"
 
 _fzf_git_fzf() {
-    fzf --height 50% --tmux 90%,70% \
-        --layout reverse --multi --min-height 20+ --border \
-        --no-separator \
-        --border-label-pos 2 \
-        --color 'label:blue' \
-        --bind 'focus:transform-preview-label:""' \
-        --bind 'ctrl-/:change-preview-window(down,50%|hidden|)' "$@"
+    fzf --bind 'ctrl-/:change-preview-window(down,50%|hidden|)' "$@"
 }
 
 _fzf_compgen_path() {
