45593ed6d2cb1bf82f696e56f2e24a15e6f39654
Matthew Dong <bruckheim@proton.me>
2025-11-23 11:57:31 +0800

    feat(nvim): Added minimal bufline

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+    'akinsho/bufferline.nvim',
+    dependencies = 'nvim-mini/mini.nvim',
+    opts = {
+        options = {
+            diagnostics = 'nvim_lsp',
+            always_show_bufferline = false,
+        },
+    },
+}
