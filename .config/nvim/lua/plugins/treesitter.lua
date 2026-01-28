bcc59789a305df1cdc3214c787f821a9b3fb4ba2
Matthew Dong <bruckheim@proton.me>
2026-01-28 15:04:34 -0500

    fix(nvim): migrate configuration to `main` branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,47 @@
+-- default parsers to install
+local parsers = {
+    'cpp',
+    'json',
+    'python',
+    'toml',
+    'regex',
+    'rust',
+    'bash',
+    'java',
+    'typst',
+    'lua',
+    'gitcommit',
+}
+
+-- autoinstall missing parsers on filetype detection
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(event)
+        local bufnr = event.buf
+
+        -- skip special buffers
+        if vim.bo[bufnr].buftype ~= '' then
+            return
+        end
+
+        -- try to start treesitter, return on success
+        local ok = pcall(vim.treesitter.start)
+        if ok then
+            return
+        end
+
+        -- install missing parser if language is supported
+        local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
+        if lang then
+            require('nvim-treesitter').install { lang }
+            pcall(vim.treesitter.start)
+        end
+    end,
+})
+
 return {
     'nvim-treesitter/nvim-treesitter',
-    event = 'VeryLazy',
-    branch = 'main',
     build = ':TSUpdate',
-    opts = {
-        ensure_installed = {
-            'cpp',
-            'json',
-            'python',
-            'toml',
-            'regex',
-            'rust',
-            'bash',
-            'java',
-            'typst',
-            'gitcommit',
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
+    init = function()
+        require('nvim-treesitter').install(parsers)
+    end,
 }
