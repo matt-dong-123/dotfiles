4315011b155b267fa28b2d17523dbefe6f192157
Matthew Dong <bruckheim@proton.me>
2026-01-31 10:49:20 -0500

    fix(treesitter): don't autoinstall parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,19 @@
--- default parsers to install
-local parsers = {
-    'cpp',
-    'json',
-    'python',
-    'toml',
-    'regex',
-    'rust',
-    'bash',
-    'java',
-    'typst',
-    'lua',
-    'gitcommit',
-}
-
--- autoinstall missing parsers on filetype detection
-vim.api.nvim_create_autocmd('FileType', {
-    callback = function(event)
-        local bufnr = event.buf
-
-        -- skip special buffers
-        if vim.bo[bufnr].buftype ~= '' then
-            return
-        end
-
-        -- try to start treesitter, return on success
-        local ok = pcall(vim.treesitter.start)
-        if ok then
-            return
-        end
-
-        -- install missing parser if language is supported
-        local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
-        if lang then
-            require('nvim-treesitter').install { lang }
-            pcall(vim.treesitter.start)
-        end
-    end,
-})
-
 return {
     'nvim-treesitter/nvim-treesitter',
     build = function()
         vim.cmd 'TSUpdate'
-        require('nvim-treesitter').install(parsers)
+        require('nvim-treesitter').install {
+            'cpp',
+            'json',
+            'python',
+            'toml',
+            'regex',
+            'rust',
+            'bash',
+            'java',
+            'typst',
+            'lua',
+            'gitcommit',
+        }
     end,
 }
