7be5ea53ecd052ce15f2c095fbb61b1940981916
Matthew Dong <bruckheim@proton.me>
2026-02-05 23:59:49 +0300

    feat(nvim): add markview.nvim

---
+++ .config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,13 @@
+return {
+    'OXY2DEV/markview.nvim',
+    ft = 'markdown',
+    dependencies = {
+        'saghen/blink.cmp',
+        'nvim-mini/mini.nvim',
+    },
+    opts = {
+        preview = {
+            icon_provider = 'mini',
+        },
+    },
+}
