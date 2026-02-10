d5dd87710c2ed4484f5f916cc62bfd543452cb54
Matthew Dong <bruckheim@proton.me>
2026-02-10 11:25:32 +0800

    fix(nvim): treesitter auto highlight

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,4 +16,15 @@ return {
             'gitcommit',
         }
     end,
+    config = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            group = vim.api.nvim_create_augroup('treesitter_highlight', {}),
+            callback = function(args)
+                local lang = vim.treesitter.language.get_lang(args.match)
+                if lang then
+                    pcall(vim.treesitter.start, args.buf, lang)
+                end
+            end,
+        })
+    end,
 }
