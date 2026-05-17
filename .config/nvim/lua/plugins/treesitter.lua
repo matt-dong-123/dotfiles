6a6566abc20ebcb0df1accc60dbe1e575ddd5ec3
Matthew Dong <bruckheim@proton.me>
2026-05-17 19:56:19 +0800

    style(nvim): use opts={}

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,18 @@
 return {
     'romus204/tree-sitter-manager.nvim',
-    dependencies = {}, -- tree-sitter CLI must be installed system-wide
-    config = function()
-        require('tree-sitter-manager').setup {
-            ensure_installed = {
-                'lua',
-                'java',
-                'cpp',
-                'c',
-                'python',
-                'html',
-                'css',
-                'javascript',
-                'go',
-                'tmux',
-                'markdown',
-            },
-        }
-    end,
+    opts = {
+        ensure_installed = {
+            'lua',
+            'java',
+            'cpp',
+            'c',
+            'python',
+            'html',
+            'css',
+            'javascript',
+            'go',
+            'tmux',
+            'markdown',
+        },
+    },
 }
