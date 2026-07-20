d32baab26684da19727870a8185304de640a64b0
Matthew Dong <bruckheim@proton.me>
2026-07-20 16:36:59 -0400

    feat(nvim): nvim-treesitter is de-archived

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,29 @@
 return {
-    'romus204/tree-sitter-manager.nvim',
-    opts = {
-        ensure_installed = {
-            'lua',
-            'java',
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
+        require('nvim-treesitter').install {
             'cpp',
-            'c',
+            'json',
             'python',
-            'html',
-            'css',
-            'javascript',
-            'go',
-            'tmux',
-            'markdown',
-        },
-    },
+            'toml',
+            'regex',
+            'rust',
+            'bash',
+            'java',
+            'typst',
+            'lua',
+            'gitcommit',
+        }
+    end,
+    config = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            group = vim.api.nvim_create_augroup('treesitter_highlight', {}),
+            callback = function(args)
+                local lang = vim.treesitter.language.get_lang(args.match)
+                if lang then
+                    pcall(vim.treesitter.start, args.buf, lang)
+                end
+            end,
+        })
+    end,
 }
